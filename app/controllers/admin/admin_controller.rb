ae55dcf4d2fbbc8430ddd4c4677ca330e936a394
Glenn Harmon <glenn.d.harmon@icloud.com>
2020-11-29 13:06:29 -0500

    landing page, scoped_admin

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Admin
+  class AdminController < ApplicationController
+    before_action :authenticate_user!
+    before_action :verify_role
+
+    AUTHORIZED_ROLE_LEVEL = ['1'].freeze
+
+    def verify_role
+      redirect_to '/' unless AUTHORIZED_ROLE_LEVEL.includes?(current_user.role.level)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+  end
 end
