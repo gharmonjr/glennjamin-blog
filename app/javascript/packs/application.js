34e95d3f1bcccf1bcb760f6d7c748f0e9891a15b
Glenn Harmon <glenn.d.harmon@icloud.com>
2020-12-07 15:17:38 -0500

    meta_tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,45 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require("trix")
-// require('script!trix')
-
-require("@rails/actiontext")
-
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-
-    // document.addEventListener('DOMContentLoaded', () => {
-    //
-    //     // Get all "navbar-burger" elements
-    //     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    //
-    //     // Check if there are any navbar burgers
-    //     if ($navbarBurgers.length > 0) {
-    //
-    //         // Add a click event on each of them
-    //         $navbarBurgers.forEach( el => {
-    //             el.addEventListener('click', () => {
-    //
-    //                 // Get the target from the "data-target" attribute
-    //                 const target = el.dataset.target;
-    //                 const $target = document.getElementById(target);
-    //
-    //                 // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    //                 el.classList.toggle('is-active');
-    //                 $target.classList.toggle('is-active');
-    //
-    //             });
-    //         });
-    //     }
-    //
-    // });
